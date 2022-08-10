62b719e0d276ba73802874ff640c5943fc9fb695
Eric VanKirk <eric@ericvankirk.com>
2022-08-10 18:40:17 -0500

    router post and get

--- Develop/apiRoutes/notesRoutes.js
+++ Develop/apiRoutes/notesRoutes.js
@@ -1,32 +1,45 @@
 const router = require('express').Router();
-const { filterByQuery, findById, createNewNote, validateNote } = require('../lib/notes');
-const { notes } = require('../data/notes');
+const fs = require('fs');
+const { notes } = require('../db/db.json');
 
 router.get('/notes', (req, res) => {
-    let results = notes;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-})
+//     let results = notes;
+//     if (req.query) {
+//         results = filterByQuery(req.query, results);
+//     }
+//     res.json(results);
+    fs.readFile('./db/db.json', 'utf8', function(err, data){
+       
+        // for (var i = 0; i < data.length; i++){
+        //     box.push(JSON.parse(data))
+        // }
+       
+        console.log(data.length);
+        res.json(JSON.parse(data));
+    })
 
-router.get('/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-})
+});
+
+// router.get('/notes/:id', (req, res) => {
+//     const result = findById(req.params.id, notes);
+//     if (result) {
+//         res.json(result);
+//     } else {
+//         res.send(404);
+//     }
+// })
 
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toStrong();
-    if (!validateNote(req.body)) {
-        res.status(400).send('Note is not properly formatted');
-    } else {
-        const note = createNewNote(req.body, notes);
-        res.json(note);
-    }
+    // fs.readFile('./db/db.json', 'utf8', function(err, data) {
+    //     console.log(data.length);
+    //     res.json(JSON.parse(data));
+    // })
+    fs.writeFile('./data.notes.json', 'utf8', function(err, data) {
+        res.json(JSON.stringify(data));
+        if (err)
+        console.log(err);
+        console.log('Your note was successfully added');
+    } )
 });
 
 module.exports = router;
