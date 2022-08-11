b0963ecf368a35ed5ed4ea8389496c4b69f6b98d
Eric VanKirk <eric@ericvankirk.com>
2022-08-10 22:41:14 -0500

    removed ./Develop path

--- apiRoutes/notesRoutes.js
+++ apiRoutes/notesRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const fs = require('fs');
-const { notes } = require('../Develop/db/db.json');
+const { notes } = require('../db/db.json');
 
 router.get('/notes', (req, res) => {
 //     let results = notes;
