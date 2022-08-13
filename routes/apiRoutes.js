e1f6fd5328fa069790506809616207e150aa40c4
Eric VanKirk <eric@ericvankirk.com>
2022-08-12 19:31:07 -0500

    reworking LA naming

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,50 @@
-const path = require('path');
 const router = require('express').Router();
+const fs = require('fs');
+const { notes } = require('../db/db.json');
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index'));
-});
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/notes'));
+router.get('./notes', (req, res) => {
+//     let results = notes;
+//     if (req.query) {
+//         results = filterByQuery(req.query, results);
+//     }
+//     res.json(results);
+    fs.readFile('../db/db.json', 'utf8', function(err, data){
+       
+        // for (var i = 0; i < data.length; i++){
+        //     box.push(JSON.parse(data))
+        // }
+       
+        console.log(data.length);
+        res.json(JSON.parse(data));
+    })
+
 });
 
+// router.get('/notes/:id', (req, res) => {
+//     const result = findById(req.params.id, notes);
+//     if (result) {
+//         res.json(result);
+//     } else {
+//         res.send(404);
+//     }
+// })
+
+router.post('./notes', (req, res) => {
+    // fs.readFile('./db/db.json', 'utf8', function(err, data) {
+    //     console.log(data.length);
+    //     res.json(JSON.parse(data));
+    // })
+    console.log(req.body);
+    const note = {
+        title: req.body.title,
+        text: req.body.text
+    }
+    fs.writeFile('./db/db.json', JSON.stringify(note), function(err) {
+        if (err)
+        console.log(err);
+        console.log('Your note was successfully added');
+        res.json('Your note was added');
+    } )
+});
 
 module.exports = router;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index'));
+});
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes'));
+});
+
+
+module.exports = router;
